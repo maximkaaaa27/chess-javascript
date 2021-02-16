e74c6f23260d77b4191c2c2ea2914fa513d9f1b1
maksimka <max_005_27@mail.ru>
2021-02-16 20:51:08 +0300

    Create class Chess and add method showBoard into them

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
 import './style.css';
 
-function showMap () {
-  let board = '<table>';
 
-    for (let y = 7; y >= 0; y--) {
-      board += '<tr>';
-       for(let x = 0; x <= 7; x++) {
-         board += '<td></td>';
-       }
-       board += '</tr>';
-    }
-    document.querySelector('#board').innerHTML = board;
+class Chess {
+
+  constructor(element) {
+    this.el = element;
+    this.showBoard();
+  }
+
+
+  showBoard () {
+    let board = '<table>';
+
+      for (let y = 7; y >= 0; y--) {
+        board += '<tr>';
+        for(let x = 0; x <= 7; x++) {
+          board += '<td></td>';
+        }
+        board += '</tr>';
+      }
+
+    this.el.innerHTML = board;
+
+  }
 }
 
-showMap();
+new Chess(document.querySelector('#board'));
+
+window.Chess = Chess;
